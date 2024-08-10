2a8733241650ba404b79f277dec331bb4127603e
Reverie <XReverieX@thenewslens.com>
2024-08-10 14:26:58 +0800

    test(game): Create bowlingGame and build test.

---
+++ 03-start/app/BowlingGame.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+class BowlingGame
+{
+    /**
+     *
+     *
+     * @param int $pins
+     * @return void
+     */
+    public function roll(int $pins): void
+    {
+
+    }
+}
--- 03-start/tests/Unit/BowlingGameTest.php
+++ 03-start/tests/Unit/BowlingGameTest.php
@@ -4,4 +4,7 @@ use App\BowlingGame;
 
 test('開局', function () {
     $game = new BowlingGame();
-});
+    for ($i = 0; $i < 20; $i++) {
+        $game->roll(0);
+    }
+})->only();
